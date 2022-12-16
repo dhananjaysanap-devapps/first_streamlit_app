a247efd532f15022b3906be7b166a2f0df5b2307
Dhananjay Sanap <120600051+dhananjaysanap-devapps@users.noreply.github.com>
2022-12-16 23:12:53 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -16,3 +16,4 @@ my_fruit_list=pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/
 streamlit.multiselect("pick some fruits:",list(my_fruit_list.index))
 #display the table on the page.
 streamlit.dataframe(my_fruit_list)
+my_fruit_list = my_fruit_list.set_index('Fruit')
