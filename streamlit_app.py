3dd33350a6ed835f4750d6902f124d966fbbfdab
Dhananjay Sanap <120600051+dhananjaysanap-devapps@users.noreply.github.com>
2022-12-16 16:25:20 +0530

    Update streamlit_app.py
    breakfast menu add
    streamlit.text and header use.

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,3 +2,7 @@
 import streamlit
 
 streamlit.title('My parent new healty dinner')
+streamlit.header('Breakfast Menu')
+streamlit.text ('Omega 3 & bulberry otmeal')
+streamlit.text ( 'Spinach & Rrocket Somuthi ')
+streamlit.text( 'Heard bolied eggs')
